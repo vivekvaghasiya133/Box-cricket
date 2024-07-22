8416ad0bd34353494ffcbbb0b1fe9c58af8ffea9
VivekInfotech <159127221+VivekInfotech@users.noreply.github.com>
2024-07-22 10:42:00 +0530

    Done

--- src/views/utilities/Typography.jsx
+++ src/views/utilities/Typography.jsx
@@ -61,20 +61,20 @@ const Typography = () => {
           formValues.append("morningPrice", values.morningPrice);
           formValues.append("nightPrice", values.nightPrice);
           
-          axios.post('http://localhost:3000/addbox', formValues, {
+          axios.post('https://box-cricket-api.onrender.com/addbox', formValues, {
             headers: {
               auth: token,
               "Content-Type": "multipart/form-data"
             }
           })
             .then((res) => {
-              // console.log(res);
+              console.log(res);
               resetForm();
               setError(null); // Clear the error if submission is successful
               setSnackbarOpen(true); // Show success message
             })
             .catch((err) => {
-              // console.log(err.response.data, "Error");
+              console.log(err, "Error");
               setError(err.response.data.message);
               setSnackbarOpen(true); // Open Snackbar on error
             });
